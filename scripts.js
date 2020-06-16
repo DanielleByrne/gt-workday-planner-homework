78e06a751d21d4040289b8ba9bb7ca22d95a3f12
Danielle Byrne <DanielleByrne421@gmail.com>
2020-06-15 23:47:34 -0400

    setting up fo add screenshot tomorrow to showcase time color coding, continued trying to get local storage to work

--- scripts.js
+++ scripts.js
@@ -1,57 +1,43 @@
 $(document).ready(function () {
-  // var currentDate = $("#currentDay")
-  var allTimeSlots = $(".timeSlot");
-  // console.log(allTimeSlots);
-  var currentDate = moment().format("MMM Do YY");
-  // console.log(currentDate);
 
-  $("#currentDay").text(currentDate);
-  var currentHour = moment().format("H");
-  // console.log(currentHour);
+// var currentDate = $("#currentDay")
+var allTimeSlots = $(".timeSlot");
+// console.log(allTimeSlots);
+var currentDate = moment().format("MMM Do YY");
+// console.log(currentDate);
 
-  allTimeSlots.each(function () {
-    var loopTime = $(this).data("time");
+$("#currentDay").text(currentDate);
+var currentHour= moment().format("H");
+// console.log(currentHour);
+
+allTimeSlots.each(function(){
+  var loopTime=($(this).data("time"));
     // console.log($(this).data("time"));
-    if (loopTime < currentHour) {
-      $(this).addClass("past");
-    } else if (loopTime > currentHour) {
-      $(this).addClass("future");
-    } else {
-      $(this).addClass("present");
+    if(loopTime < currentHour){
+      $(this).addClass("past")
     }
-  });
-  // Local Storage
-  // saving info from the input .textarea
-  //   //listening for button click on the save button .saveBtn
-  var textEl = $("#input15").val();
+    else if(loopTime > currentHour){
+      $(this).addClass("future")
+    }
+    else {
+      $(this).addClass("present")
 
-  var rows = $(".row").each(function () {
-    $(".saveBtn").on("click", function () {
-      var textInput = $(".textarea").val();
-      console.log(textInput)
-    });
-  });
+    }
+})
 
-  // console.log(rows);
+// var textEl = $("#input15").val();
+
+
+var rows = $(".row").each(function () {
+  $(".saveBtn").on("click", function () {
+    var textInput = $(".textarea").val();
+    console.log(textInput)
+  });
+  // localStorage.setItem (".textarea",JSON.stringify(textInput));
+  // var lastInput = JSON.parse(localStorage.getItem(".textarea"));
+});
+
+
 
 });
 
-//  PSEUDOCODE
-
-// ```
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
-//Things I need to add :
-// make it so that the save buttons save the user input into local storage
